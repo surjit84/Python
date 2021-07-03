058158f0edea28bb3b2b81ba8bd6206431a6ab5b
DESKTOP-EO1L09Q\surji <surjit84.singh@gmail.com>
2021-07-03 16:20:44 +0530

    Eg of Z Test

--- Hypothesis_Test.py
+++ Hypothesis_Test.py
@@ -11,3 +11,20 @@
 # f Test: Comparison of variance
 # ANOVA Test: Analysis of Variance Test
 # Chi2 Test: Comparision/Correlation between two categorical Variables
+
+# Point Estimate : Value of any statistics that estimate value of a parameter is called point estimate.
+# Confidence Interval = Point Estimate +/- Margin of Error
+
+# Eg: On Verbal Test of CAT exam, sd = 100,  
+#25 test taken, they have mean of 520, Construct a 95% CI about mean
+
+#sd = 100, n = 25, alpha = .05, CI = 95%
+
+# = 520 + Z(0.025)*(100/root(25))
+# = 520 + 1.96*(100/root(25)) = 559.2
+# = 520 - Z(0.025)*(100/root(25))
+# = 520 - 1.96*(100/root(25)) = 480.2
+
+# So if value is between 480.2 to 559.2 then we will accept H0 else H1 with 95% CI.
+
+
