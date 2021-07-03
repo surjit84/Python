03c8cd6253f85b3461b64475417fdb1340869f81
DESKTOP-EO1L09Q\surji <surjit84.singh@gmail.com>
2021-07-03 15:58:39 +0530

    Added  Type of Tests

--- Hypothesis_Test.py
+++ Hypothesis_Test.py
@@ -4,3 +4,10 @@
 # if p value falls in CI region then accept H0 (null hypothesis)
 # if p value is outside of CI region the we will reject H0, and accept alternate hypothesis H1.
 # p value is outcome of statistical experiment and aplha value is given by domain expert
+
+# Type of Tests
+# Z Test Comparison of Mean
+# T Test: Comparison of Mean
+# f Test: Comparison of variance
+# ANOVA Test: Analysis of Variance Test
+# Chi2 Test: Comparision/Correlation between two categorical Variables
