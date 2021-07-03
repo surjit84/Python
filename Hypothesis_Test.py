68ea32045ac0c890310a2ba2d8fbedf0e94d7c16
DESKTOP-EO1L09Q\surji <surjit84.singh@gmail.com>
2021-07-03 15:51:06 +0530

    Stats Hypothesis Testing

---
+++ Hypothesis_Test.py
@@ -0,0 +1,6 @@
+# significance value = alpha (decided by domain expert)
+# Confidence interval CI based on aplha value/
+# perform p value based on statistical analysis
+# if p value falls in CI region then accept H0 (null hypothesis)
+# if p value is outside of CI region the we will reject H0, and accept alternate hypothesis H1.
+# p value is outcome of statistical experiment and aplha value is given by domain expert
